3b2963747fa02922af2f091d1ba9eb6dae02d874
Bárbara <barbaradd33@gmail.com>
2026-04-23 11:27:37 -0400

    hice la clase user para el login

---
+++ DonacionONG/model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DonacionONG.model
+{
+    internal class User
+    {
+        private String Username;
+        private String Password;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username1 { get => Username; set => Username = value; }
+        public string Password1 { get => Password; set => Password = value; }
+    }
+
+
+    
+}
